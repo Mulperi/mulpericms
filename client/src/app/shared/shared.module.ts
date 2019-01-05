544e15b24f9709f421e6eaf528c328116ae246a8
Mika Lakanen <mikalakanen@hotmail.com>
2019-01-05 14:20:45 +0200

    Add Header component. Adjust styles.

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
+import { HeaderComponent } from './components/header/header.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, HeaderComponent],
   imports: [RouterModule],
-  exports: [ToolbarComponent]
+  exports: [ToolbarComponent, HeaderComponent]
 })
 export class SharedModule {}
