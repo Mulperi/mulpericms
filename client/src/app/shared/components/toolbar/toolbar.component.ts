f7f548e5ee1a32a4de564e7e5d6157881581ee92
Mika Lakanen <mikalakanen@hotmail.com>
2019-01-05 17:54:41 +0200

    Add loading spinner component.

--- client/src/app/shared/components/toolbar/toolbar.component.ts
+++ client/src/app/shared/components/toolbar/toolbar.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  authenticated = false;
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
-import { HeaderComponent } from './components/header/header.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
 @NgModule({
-  declarations: [ToolbarComponent, HeaderComponent],
-  imports: [RouterModule],
-  exports: [ToolbarComponent, HeaderComponent]
+  declarations: [ToolbarComponent, HeaderComponent, SpinnerComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [ToolbarComponent, HeaderComponent, SpinnerComponent]
 })
 export class SharedModule {}
